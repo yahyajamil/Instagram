3eeb59bb003659b84bdcd40534dd9be6da68dea2
Yahya Jamil <91267538+yahyajamil@users.noreply.github.com>
2023-12-08 21:48:22 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import i18n from "i18next";
 import {initReactI18next } from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
+import { HashRouter as Router } from 'react-router-dom';
 
 import App from './Component/App';
 import './Component/style.css';
@@ -16,20 +17,29 @@ i18n
   .use(HttpApi)
   .init({
     fallbackLng: "en",
-    detection : {
+    detection: {
       order: ['path', 'htmlTag', 'cookie', 'localStorage', 'subdomain'],
       caches: ['cookie']
     },
     backend: {
       loadPath: '/assets/locales/{{lng}}/translation.json',
     },
-    react: { useSuspense : false}
+    react: { useSuspense: false }
+  })
+  .then(() => {
+    console.log('i18n initialized successfully!');
+  })
+  .catch(error => {
+    console.error('Error initializing i18n:', error);
   });
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Router>
+        <App />
+    </Router>
 );
 
  
