dff0cb4103fc6d17503bb14417ce866a8e98276b
Yahya Jamil <91267538+yahyajamil@users.noreply.github.com>
2023-12-11 21:27:29 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import {initReactI18next } from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
 import App from './Component/App';
 import './Component/style.css';
 
+
+
 i18n
-  .use(initReactI18next)
+  .use(initReactI18next) 
   .use(LanguageDetector)
   .use(HttpApi)
   .init({
-    fallbackLng: 'en',
-    detection: {
+    fallbackLng: "en",
+    detection : {
       order: ['path', 'htmlTag', 'cookie', 'localStorage', 'subdomain'],
-      caches: ['cookie'],
+      caches: ['cookie']
     },
     backend: {
-      loadPath: '/locales/{{lng}}/translation.json',
+      loadPath: '/assets/locales/{{lng}}/translation.json',
     },
-    react: { useSuspense: false },
+    react: { useSuspense : false}
   });
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
 );
 
+ 
